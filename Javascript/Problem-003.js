368a2480104aefa6385f9702346f01bc13bd3cc3
finnishmiko <finnishmiko@hotmail.com>
2015-11-28 20:02:53 +0200

    Fixed equation

--- Javascript/Problem-003.js
+++ Javascript/Problem-003.js
@@ -7,35 +7,50 @@ The prime factors of 13195 are 5, 7, 13 and 29.
 What is the largest prime factor of the number 600851475143 ?
 
 */
-
 var number=600851475143;
+//var number=600851475;
+var divNumber=number;
 var maxsize=Math.floor(Math.sqrt(number));
-console.log("Start from: ", maxsize);
-var prime=1, answer=0;
+//console.log("Start from: ", maxsize);
+var answer=0;
+var divisor=[];
+var index=0;
 
-var isPrime = function (number) {
-    for (var i = 2; i<number; i++){
-        if(number%i==0){
-            console.log(i, "is not prime number");
-            return 0;
-        }
+//function to test if testNumber is prime number
+var isPrime = function (testNumber) {
+    for (var j = 2; j<testNumber; j++){
+      if(testNumber%j==0){return 0;}
     }
-        console.log("prime");
-        console.log(i);
-        return number;
+      return testNumber;
 };
 
-for (var i = maxsize; i > 0; i--) {
-    if(number%i==0){
-        console.log("Number", i, "is divisible with", number);
+// Find divisors that are below number's square root
+for (var i = maxsize; i > 1; i--) {
+    while(divNumber%i==0){    
+        //console.log("Number", i, "is divisible with", divNumber); 
         answer=isPrime(i);
-        if(answer!==0){break;}
+        if(answer!==0){
+          //console.log(answer, "is prime");
+          divisor[index]=answer; 
+          index++;
+          divNumber=divNumber/answer;
+        }
+        else{
+          //console.log(i, "is not prime number"); 
+          break;
+        }
     }
 }
-    console.log("vastaus");
-    console.log(answer);
+    console.log("Divisors");
+    console.log(divisor);
 
+// Check largest divisor as it can be larger than SQRT(number)
+for(var k=0; k<divisor.length; k++){
+    number=number/divisor[k];
+    console.log(number);
+}
+if (number===1){answer=divisor[0];}
+else {answer=number}
 console.log("Answer is:");
 console.log(answer);
-
 /*Answer is 6857*/
